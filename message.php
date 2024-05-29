94d91327222bf46193c7fec8e6ceafb6859ac402
elodie <elodie.mathurin@edu.ece.fr>
2024-05-29 19:50:35 +0200

    Modifications: - chatroom: affichage des messages + style ?

--- message.php
+++ message.php
@@ -6,6 +6,11 @@ $database = "sportify";
 $db_handle = mysqli_connect('localhost', 'root', '' );
 $db_found = mysqli_select_db($db_handle, $database);
 
+$me = 132645; ///session
+$sql = "SELECT * FROM coach";
+$result = mysqli_query($db_handle, $sql);
+$conn = new mysqli('localhost', 'root', '',$database);
+
 
 ?>
 
@@ -13,45 +18,60 @@ $db_found = mysqli_select_db($db_handle, $database);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
     <title>Chatroom</title>
-</head>
-<body>
-<table>
 
-    <form action = "" method = "post">
+    <style>
 
-        <input type = "text" name = "message" required>
-        <INPUT TYPE = "Submit" Name = "Submit1" VALUE = "Envoyer"> <br>
-        <label for="Coach">Choose a Coach:</label>
-        <select id="Coach" name="Coach">
-
-
-
-        <?php
-        $me = 132645; ///session
-        $sql = "SELECT * FROM coach";
-	 	$result = mysqli_query($db_handle, $sql);
-        $conn = new mysqli('localhost', 'root', '',$database);
-
-         ///affichage nom coach
-        while ($data = mysqli_fetch_assoc($result)){
-
-            echo'
-            <option value="'.$data["Nom"].'">'. $data["Nom"].'</option>
-            ';
+        p, h1{
+            text-align: center;
         }
 
-        $coach = isset($_POST["Coach"])? mysqli_real_escape_string($db_handle, $_POST["Coach"]) : "";
-        $mess = isset($_POST["message"])? mysqli_real_escape_string($db_handle, $_POST["message"]) : "";
+        .recu{
+            color:white;
+            background: gray;
+            margin-bottom: 10px;
+            width: 100px;
+            float: left;
+            margin-bottom: 10px;
+            margin-right: 200px;
+            width: 100px;
+            clear: both;
+            border-radius: 50px;
+        }
 
-?>
-    </form>
-</table>
+        .send{
+            color:white;
+            background: blueviolet;
+            margin-bottom: 10px;
+            margin-right: 200px;
+            width: 100px;
+            float:right;
+            clear: both;
+            border-radius: 50px;
+        }
+
+        .chat{
+            margin:auto;
+            background-color: black;
+        }
+
+        form{
+            margin-right: auto;
+            margin-left: auto;
+            clear: both;
+        }
+    </style>
+</head>
+<body>
 
     <?php
+    $coach = isset($_POST["Coach"])? mysqli_real_escape_string($db_handle, $_POST["Coach"]) : "";
+    $mess = isset($_POST["message"])? mysqli_real_escape_string($db_handle, $_POST["message"]) : "";
 
 
-        $sql1 = "INSERT INTO message (source, dest, message) 
+
+    $sql1 = "INSERT INTO message (source, dest, message) 
 Values ('$me','$coach','$mess')";
 
 
@@ -72,6 +92,7 @@ Values ('$me','$coach','$mess')";
 
 <h1> Messages </h1>
 
+<div class="chat">
 <?php
 
 
@@ -82,21 +103,47 @@ $result3 = mysqli_query($db_handle, $sql3);
 
 while($data = mysqli_fetch_assoc($result2)){//messages envoyé
     echo"
-    <p style ='color:red'>".$data['message']."</p>
+    <p class = 'send'>".$data['message']."</p>
     ";
 
 }
 
 while($data = mysqli_fetch_assoc($result3)){//messages reçu
     echo"
-    <p style ='color:blue'>".$data['message']."</p>
+    <p class='recu'>".$data['message']."</p>
     ";
 
 }
 
 ?>
 
+    <table>
 
+        <form action = "" method = "post">
+
+            <input type = "text" name = "message" required>
+            <INPUT TYPE = "Submit" Name = "Submit1" VALUE = "Envoyer"> <br>
+            <label for="Coach">Choose a Coach:</label>
+            <select id="Coach" name="Coach">
+
+
+
+                <?php
+
+                ///affichage nom coach
+                while ($data = mysqli_fetch_assoc($result)){
+
+                    echo'
+            <option value="'.$data["Nom"].'">'. $data["Nom"].'</option>
+            ';
+                }
+
+
+                ?>
+        </form>
+    </table>
+
+</div>
 
 </body>
 </html>
